aed5a7101982d88aad1ce31fce00cfc0d8c7bf50
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2020-05-06 10:00:17 -0300

    feat: export index

--- src/vpn.ts
+++ src/vpn.ts
@@ -1,33 +1,33 @@
 import { Options } from './interfaces';
 
-const $ = require('nodobjc'); // eslint-disable-line
+export const $ = require('nodobjc'); // eslint-disable-line
 
 $.import('./vendor/VPNManager.framework');
 
 export class Bridge {
-  private manager: any;
+  private vpnManager: any;
 
   constructor(options: Options) {
     this.create(options);
   }
 
   /* get isConnected(): boolean {
-    return $.VPNManager('isConnected');
+    return this.vpnManager('isConnected');
   } */
+
+  get manager(): any {
+    return this.vpnManager;
+  }
 
   create(options: Options): this {
     const json = JSON.stringify(options);
-    this.manager = $.VPNManager('alloc')('initWithJson', $(json));
+    this.vpnManager = $.VPNManager('alloc')('initWithJson', $(json));
 
     return this;
   }
 
-  getManager(): any {
-    return this.manager;
-  }
-
   connect(username: string, password: string): boolean {
-    this.manager('connect', $(username), 'password', $(password));
+    this.vpnManager('connect', $(username), 'password', $(password));
 
     return true;
   }
